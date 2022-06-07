072480eadd5030c032228a49920266423aef2069
brunbs <babosa227@gmail.com>
2022-06-07 18:43:24 -0300

    Add module study

---
+++ JavaScript/modules/main.mjs
@@ -0,0 +1,5 @@
+import { showAge, showCity, showHobby } from "./functions.mjs";
+
+console.log(showAge('Bruno', 31));
+console.log(showCity('Bruno', 'Porto Alegre'));
+console.log(showHobby('Bruno', 'Estudar Programação'));
