cd4657012a6eeb30859215152a7f1ef1aa815c53
brunbs <babosa227@gmail.com>
2022-06-10 20:29:45 -0300

    Add PUT method to alter course

--- Angular/primeira-aplicacao/course-manager/src/app/courses/course-info.component.ts
+++ Angular/primeira-aplicacao/course-manager/src/app/courses/course-info.component.ts
@@ -22,6 +22,9 @@ export class CourseInfoComponent implements OnInit {
     }
 
     save(): void {
-        this.courseService.save(this.course);
+        this.courseService.save(this.course).subscribe({
+            next: course => console.log('Saved with success', course),
+            error: err => console.log('Erro', err)
+        });
     }
 }
