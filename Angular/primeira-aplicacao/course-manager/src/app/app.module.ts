9bc420a3c880ac12717cc1353c8b7e0f9efd3045
brunbs <babosa227@gmail.com>
2022-06-10 23:25:40 -0300

    Add core module

--- Angular/primeira-aplicacao/course-manager/src/app/app.module.ts
+++ Angular/primeira-aplicacao/course-manager/src/app/app.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar-component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Error404Component
   ],
   imports: [
@@ -19,6 +18,7 @@ import { CourseModule } from './courses/course.module';
     FormsModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
