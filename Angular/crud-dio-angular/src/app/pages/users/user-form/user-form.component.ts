5d0cc97625315b94edfff6295ea91a5e84f996e6
brunbs <babosa227@gmail.com>
2022-06-16 11:01:00 -0300

    Create user post method

--- Angular/crud-dio-angular/src/app/pages/users/user-form/user-form.component.ts
+++ Angular/crud-dio-angular/src/app/pages/users/user-form/user-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-form',
@@ -8,9 +10,10 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class UserFormComponent implements OnInit {
 
+  users: Array<User> = [];
   userForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private userService: UserService) {
     this.userForm = this.fb.group({
       id: 0,
       nome: '',
@@ -23,8 +26,17 @@ export class UserFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getUsers(): void {
+    this.userService.getUsers().subscribe(response => {
+      this.users = response;
+    })
+  }
+
   createUser(): void {
-    
+    this.userForm.get('id')?.patchValue(this.users.length + 1);
+    this.userService.postUser(this.userForm.value).subscribe(result => {
+      console.log(`Usuário ${result.nome} ${result.sobrenome} cadastrado com sucesso`);
+    })
   }
 
 }
--- Angular/crud-dio-angular/src/app/services/user.service.ts
+++ Angular/crud-dio-angular/src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
@@ -9,11 +9,20 @@ import { User } from '../models/user';
 export class UserService {
 
   apiUrl = 'https://sheet.best/api/sheets/b0030ce0-6c06-4777-ac40-e8e44683b158';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
 
   constructor(private httpClient: HttpClient) { }
 
   getUsers(): Observable<User[]> {
     return this.httpClient.get<User[]>(this.apiUrl);
+  }
+
+  postUser(user: User): Observable<User> {
+    return this.httpClient.post<User>(this.apiUrl, user, this.httpOptions);
   }
 
 }
