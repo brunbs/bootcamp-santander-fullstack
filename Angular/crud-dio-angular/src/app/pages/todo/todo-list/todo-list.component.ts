28e89b148f344bc8c6eedecf0c96806bdc6d6f75
brunbs <babosa227@gmail.com>
2022-06-17 16:48:01 -0300

    Using local storage do storage todos

--- Angular/crud-dio-angular/src/app/pages/todo/todo-list/todo-list.component.ts
+++ Angular/crud-dio-angular/src/app/pages/todo/todo-list/todo-list.component.ts
@@ -18,7 +18,13 @@ export class TodoListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
+    let items: any = localStorage.getItem('todos');
+    let todos = JSON.parse(items);
+    if(!todos) {
+      this.todos = [];
+    } else {
+      this.todos = todos;
+    }
   }
 
   addTodo(title: string) {
@@ -28,12 +34,13 @@ export class TodoListComponent implements OnInit {
       title: title,
       done: false
     });
-    console.log(this.todos);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 
   removeTodo(todo: any): void {
     let index = this.todos.indexOf(todo);
     this.todos.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 
 }
